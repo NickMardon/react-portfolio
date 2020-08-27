289fa05b95b895c2011522e9c4b865707e406e6e
Nick Mardon <nickmardondigital@gmail.com>
2020-08-27 16:50:22 -0700

    nav is rendering to page

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Nav() {
+    return (
+        <ul className="nav">
+            <li className="nav-item">
+                <a className="nav-link active" href="#">Active</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li>
+        </ul>
+    )
+}
