bc10a4397fa228c3d5b8e705aead240dd79c9840
Nick Mardon <nickmardondigital@gmail.com>
2020-08-28 11:25:55 -0700

    added more components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer class="footerBox">
+            <div class="footerContainer">
+                <p id="footerText">Copyright &#169; 2020 Nick Mardon Digital</p>
+            </div>
+        </footer>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Nav from "../components/Nav/Nav";
+import Jumbotron from "../components/Jumbotron/Jumbotron";
+import Footer from "../components/Footer/Footer";
 
 export default function Home(){
     return (
+        <div>
         <Nav />
+        <Jumbotron />
+        <Footer />
+        </div>
     )
 }
